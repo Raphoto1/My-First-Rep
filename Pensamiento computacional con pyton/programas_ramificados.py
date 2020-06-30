e2313553abeabc9ea805ac996999f873d129caa5
Raphoto1 <rrhhmmtt@gmail.com>
2020-06-30 11:35:51 -0500

    primer intento

---
+++ Pensamiento computacional con pyton/programas_ramificados.py
@@ -0,0 +1,8 @@
+num_1 = int(input('escoge un entero: '))
+num_2 = int(input('escoge otro entero'))
+if num_1 > num_2:
+    print('el primer numero es mayor')
+    elif num_1<num_2:
+        print('el segundo numero es mayor')
+        else:
+            print('los dos numeros son iguales')
